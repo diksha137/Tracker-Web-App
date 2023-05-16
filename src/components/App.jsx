100098cd6a21dcf03693ae68b2b31e60bc7cf5c9
diksha137 <diksha.id202@gmail.com>
2023-05-16 18:38:08 +0530

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React from "react"
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Note from "./Note.jsx"
+import notes from "../notes.js"
 
+function createNote(notes){
+    return (
+        <Note key={notes.key} title={notes.title} content={notes.content}/>
+    )
+}
+
+var task=true;
 function App(){
     return (
         <div>
+           
             <Header/>
-            <Footer/>
-            <Note/>
+            {notes.map(createNote)};
+            <Footer/> 
+        
+           {/* {task ? document.write(<strike> buy milk </strike>) : document.write(<h1>buy milk</h1>)} */}
 
-        </div>
-    );
+           </div>
+        
+    )
 }
 
 export default App;
+
+
+// import React from "react";
+// import Header from "./Header";
+// import Footer from "./Footer";
+// // import Note from "./Note";
+// // import notes from "../notes";
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       {/* {notes.map(noteItem => (
+//         <Note
+//           key={noteItem.key}
+//           title={noteItem.title}
+//           content={noteItem.content}
+//         />
+//       ))}; */}
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
